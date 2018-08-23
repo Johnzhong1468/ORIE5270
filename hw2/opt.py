f670a8d81c8e5a10ad825de2bc9ffedc438e251d
Johnzhong1468 <0429zhongchen@gmail.com>
2018-08-23 09:52:39 -0400

    changed syntax

--- hw2/opt.py
+++ hw2/opt.py
@@ -2,30 +2,35 @@ from scipy.optimize import minimize
 import random
 import numpy as np
 
+
 def rb(x):
     """
     Rosenbrock function
     :param x: array(1xn)
+    :return calculated real value
     """
     f_sum = 0
     for i in range(len(x)-1):
         f_sum += 100*(x[i+1]-x[i]**2)**2+(1-x[i])**2
     return f_sum
-    
+
+
 if __name__ == '__main__':
     random.seed(0)
-    print("Using 'L-BFGS-B' and choose different starting points with values from -5 to 5: \n")
+    print("Using 'L-BFGS-B' and choose different starting \
+        points with values from -5 to 5: \n")
     for i in range(5):
-        start = random.sample(list(np.linspace(-5,5,100)),3)
-        result = minimize(rb,start,method = 'L-BFGS-B')
-        print("Start point is: ",start)
-        print("Minimized value: ",result.fun)
-        print("Minimizing point: ",result.x,"\n")
+        start = random.sample(list(np.linspace(-5, 5, 100)), 3)
+        result = minimize(rb, start, method='L-BFGS-B')
+        print("Start point is: ", start)
+        print("Minimized value: ", result.fun)
+        print("Minimizing point: ", result.x, "\n")
 
-    print("\n\nUsing 'BFGS' and choose different starting points with values from -5 to 5: \n")
+    print("\n\nUsing 'BFGS' and choose different starting \
+        points with values from -5 to 5: \n")
     for i in range(5):
-        start = random.sample(list(np.linspace(-5,5,100)),3)
-        result = minimize(rb,start,method = 'BFGS')
-        print("Start point is: ",start)
-        print("Minimized value: ",result.fun)
-        print("Minimizing point: ",result.x,"\n")
+        start = random.sample(list(np.linspace(-5, 5, 100)), 3)
+        result = minimize(rb, start, method='BFGS')
+        print("Start point is: ", start)
+        print("Minimized value: ", result.fun)
+        print("Minimizing point: ", result.x, "\n")
